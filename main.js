28fce952e685f4672c58bb43e73d9ec7261c5d7a
Apaar Madan <apaar_madan@hotmail.com>
2023-09-21 01:47:51 -0400

    Shadow bunny

--- main.js
+++ main.js
@@ -14,11 +14,19 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 scene.background = new THREE.Color(0x79c6d4);
 
 const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
 
 dirLight.position.set(1, 1, 0);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 1024;
+dirLight.shadow.mapSize.height = 1024;
+dirLight.shadow.camera.near = 0.5;
+dirLight.shadow.camera.far = 500;
 
 scene.add(dirLight);
 scene.add(new THREE.AmbientLight(0xfcebc3, 0.1));
@@ -29,12 +37,15 @@ const loader = new GLTFLoader();
 let bunny = null;
 
 loader.load("public/bunny.glb", function (gltf) {
-  scene.add(gltf.scene);
   bunny = gltf.scene.children[0];
+  bunny.castShadow = true;
+  scene.add(bunny);
 });
 
 loader.load("public/ground.glb", function (gltf) {
-  scene.add(gltf.scene);
+  const ground = gltf.scene.children[0];
+  ground.receiveShadow = true;
+  scene.add(ground);
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
